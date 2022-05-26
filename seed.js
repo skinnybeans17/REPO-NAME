bc7621730817c263250afa6223cb36f298bcac36
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2022-05-26 11:07:51 -0700

    seeds

--- seed.js
+++ seed.js
@@ -1,6 +1,6 @@
 process.env.NODE_ENV = "dev"
 module.exports = {
-    "undefined": "localhost/petes-pets-master",
-    "dev": "localhost/petes-pets-master",
-    "prod": "localhost/petes-pets-master"
-}
+    "undefined": "localhost/local",
+    "dev": "localhost/local",
+    "prod": "localhost/local"
+};
