ad4fb94779730ff007b325391a4937d5b103377c
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2022-06-09 13:19:49 -0700

    6/9

--- seed.js
+++ seed.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "undefined": "localhost/local",
-    "dev": "localhost/local",
-    "prod": "localhost/local"
+    "undefined": "localhost/petes-pets",
+    "dev": "localhost/petes-pets",
+    "prod": "localhost/petes-pets"
 };
