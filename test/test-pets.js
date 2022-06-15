d9238ab965700b84c7397141e8b21a155fbc7f2c
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2022-06-15 11:57:30 -0700

    Implemented responding to JSON

--- test/test-pets.js
+++ test/test-pets.js
@@ -126,5 +126,17 @@ describe('Pets', ()  => {
           res.should.be.html;
           done();
         });
+      });
+    });
+
+it('should list ALL pets on /pets GET', function(done) {
+  chai.request(server)
+      .get('/')
+      .set('content-type', 'application/json')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        done();
+      });
   });
-});
